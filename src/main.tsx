b158fd957fb3092eecb276a3e20492768c003ff9
devbyenzo <mcpigle@gmail.com>
2026-05-11 17:18:49 -0300

    add: Smooth Scrool

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 
+import Lenis from '@studio-freight/lenis'
+
+const lenis = new Lenis({
+  duration: 1.2,
+  smoothWheel: true,
+})
+
+function raf(time: number) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
